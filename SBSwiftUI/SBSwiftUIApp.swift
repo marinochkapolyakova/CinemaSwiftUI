373560b7b76185032daa80e4fe789f7ea37ed49b
Marina <111554076+marinochkapolyakova@users.noreply.github.com>
2023-03-18 14:39:46 +0300

    Initial Commit

---
+++ SBSwiftUI/SBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SBSwiftUIApp.swift
+//  SBSwiftUI
+//
+//  Created by marinapolyakova on 18.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
